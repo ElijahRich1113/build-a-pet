fbfa771969994181534107cbb35fbebca5ab26dd
ElijahRich1113 <erichards2508@myhta.org>
2018-04-17 14:00:52 -1000

    saving...

---
+++ app.js
@@ -0,0 +1,17 @@
+/*var person = {
+    person.name = "Aaron";
+    person.age = 15;
+    person.lives = "Hawaii";
+}
+
+person.height = "5 foot 10 inches";
+
+person.sayName = function(){
+    console.log("Hello" + " " + person.name);
+}
+
+var obj = {};
+obj.color = "blue";
+
+*/
+
