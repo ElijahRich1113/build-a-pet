535ddbd5f9dcaaed55347dd093b4ab175ea7599d
ElijahRich1113 <erichards2508@myhta.org>
2018-04-24 13:51:49 -1000

    need help console logging

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-/*var person = {
-    person.name = "Aaron";
-    person.age = 15;
-    person.lives = "Hawaii";
-}
+window.onload = function() {
+    var form = document.getElementById("form");
+    var animalName = document.getElementById("name");
+    var typePet = document.getElementById("type");
+    var numberLegs = document.getElementsByClassName("legs");
+    var petEmail = document.getElementById("email");
+    var petPassword = document.getElementById("password");
+    var petDescription = document.getElementById("desc");
+    var sA = document.getElementsByClassName("s-a");
+    var obj = {};
 
-person.height = "5 foot 10 inches";
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        obj.name = animalName.value;
+        obj.type = typePet.value;
+        obj.legs = numberLegs.value;
+        obj.spec = sA.value;
+        obj.email = petEmail.value;
+        obj.pass = petPassword.value;
+        obj.desc = petDescription.value;
+        console.log(obj);
+    });
+};
 
-person.sayName = function(){
-    console.log("Hello" + " " + person.name);
-}
+// var obj = {};
 
-var obj = {};
-obj.color = "blue";
-
-*/
-
+// console.log("obj", obj);
+// obj.petName = "Shockwave";
+// console.log("obj", obj);
